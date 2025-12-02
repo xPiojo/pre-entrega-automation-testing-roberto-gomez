fdbd0f4d248c835a069d4d5f8503a0f031c3f0da
xPiojo <95gomezroberto@gmail.com>
2025-12-02 20:04:15 -0300

    Add LoginPage with login and error handling

---
+++ pages/login_page.py
@@ -0,0 +1,38 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class LoginPage:
+    URL = "https://www.saucedemo.com/"
+
+    _USER_INPUT = (By.ID,"user-name")
+    _PASS_INPUT = (By.ID,"password")
+    _LOGIN_BUTTON = (By.ID, "login-button")
+    _ERROR_MSG = (By.CSS_SELECTOR, "h3[data-test='error']")
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+
+    def open(self):
+        self.driver.get(self.URL)
+
+    def login(self, username, password):
+        user_input = self.wait.until(EC.visibility_of_element_located(self._USER_INPUT))
+        pass_input = self.driver.find_element(*self._PASS_INPUT)
+        login_button = self.driver.find_element(*self._LOGIN_BUTTON)
+
+        user_input.clear()
+        user_input.send_keys(username)
+        pass_input.clear()
+        pass_input.send_keys(password)
+        login_button.click()
+    
+    def get_error_message(self):
+        error_element = self.wait.until(
+            EC.visibility_of_element_located(self._ERROR_MSG)
+        )
+        return error_element.text
+
